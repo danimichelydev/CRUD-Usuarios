e8aacb38f6c8e650eeff985f29dceff4e225253b
Dani Michely <danielamichelydev@gmail.com>
2022-07-20 11:52:59 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,76 @@
+'use strict'
+
+const openModal = () => document.getElementById('modal')
+    .classList.add('active')
+
+const closeModal = () => {
+  clearFields()
+  document.getElementById('modal').classList.remove('active')
+}
+
+
+
+const tempClient = {
+  nome: "michely",
+  email: "michely@gmail.com",
+  celular: "1111-1111",
+  cidade: "Recife"
+}
+
+const getLocalStorage = () => JSON.parse(localStorage.getItem('db_client')) ?? []
+const setLocalStorage = (dbClient) => localStorage.setItem("db_client", JSON.stringify(dbClient))
+
+
+const deleteClient = (index) => {
+  const dbClient = readClient()
+  dbClient.splice(index, 1 )
+  setLocalStorage(dbClient)
+}
+
+const updateClient = (index, client) => {
+  const dbClient = readClient()
+  dbClient[index] = client
+  setLocalStorage(dbClient)
+}
+
+const readClient = () => getLocalStorage()
+
+const createClient = (client) => {
+  const dbClient = getLocalStorage()
+  dbClient.push (client)
+  setLocalStorage(dbClient)
+}
+
+const isValidFields = () => {
+  return document.getElementById("form").reportValidity()
+}
+
+// Interação com o layout
+const clearFields = () => {
+  const fields = document.querySelectorAll(".modal-field")
+  fields.forEach(field => field.value = "")
+}
+
+const saveClient = () => {
+  if(isValidFields()){
+    const client = {
+      nome: document.getElementById("nome").value,
+      email: document.getElementById("email").value,
+      celular: document.getElementById("celular").value,
+      cidade: document.getElementById("cidade").value,
+    }
+    createClient(client)
+    clearFields()
+    closeModal()
+  }
+}
+
+// eventos 
+document.getElementById('cadastrarCliente')
+    .addEventListener('click', openModal)
+
+document.getElementById('modalClose')
+    .addEventListener('click', closeModal)
+
+document.getElementById("salvar")
+  .addEventListener("click", saveClient)
